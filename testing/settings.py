7235ef94789dc816906e1ddb5a6a097efa6da85f
ashish <ashu.nim15@gmail.com>
2018-10-30 16:54:00 +0530

    changes made

--- testing/settings.py
+++ testing/settings.py
@@ -139,4 +139,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 LOGIN_REDIRECT_URL = 'authorize:home'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
